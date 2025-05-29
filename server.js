62bb7652f30b3cdae9bfdb2d634be047fa33843c
GowthamPoongan <gowthampoongan@gmail.com>
2025-05-29 20:57:28 +0530

    Fix: update server.js to listen on port 80 and 0.0.0.0

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 80;
 app.use(express.static('public'));
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Portfolio app running on http://localhost:${PORT}`);
+  console.log(`Portfolio app running on http://0.0.0.0:${PORT}`);
 });
